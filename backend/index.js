33e96109ff69e78aa8eefac358a3be3d7aa30c6d
russhenson <49965783+russhenson@users.noreply.github.com>
2023-07-01 21:15:42 +0800

    updated

--- backend/index.js
+++ backend/index.js
@@ -18,4 +18,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // use router
 app.use(Router);
 
-app.listen(5000, () => console.log("Server running..."));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
+
